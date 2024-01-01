761c467c2c1899bda89caca945a3ff21c3db3d2f
nick <devknightruin@outlook.com>
2023-12-31 18:29:01 -0600

    integrating React-HookForms and eslint

--- src/App_ExpenseTracker.tsx
+++ src/App_ExpenseTracker.tsx
@@ -1,33 +1,31 @@
-import './styles/index_ET.css'
-import { useState } from 'react'
-import {DataInfo} from './assets/Utilities/interfaceTypes'
-import Form from './assets/tracker_components/Form'
-import Table from './assets/tracker_components/Table'
+import "./styles/index_ET.css";
+import { useState } from "react";
+import { DataInfo } from "./assets/Utilities/interfaceTypes";
+import Form from "./assets/tracker_components/Form";
+import Table from "./assets/tracker_components/Table";
 
-function App_ExpenseTracker(){
-    const [itemList, setItemList] = useState<DataInfo[]>([
-        // {itemName: "test 1", cost: 178, Category: "Clothes", info: "testing data"},
-        // {itemName: "test 2", cost: 278, Category: "Entertainment", info: "testing data"},
-        // {itemName: "test 3", cost: 878, Category: "Clothes", info: "testing data"}
-    ])
+function App_ExpenseTracker() {
+	const [itemList, setItemList] = useState<DataInfo[]>([
+		// {itemName: "test 1", cost: 178, Category: "Clothes", info: "testing data"},
+		// {itemName: "test 2", cost: 278, Category: "Entertainment", info: "testing data"},
+		// {itemName: "test 3", cost: 878, Category: "Clothes", info: "testing data"}
+	]);
 
-    const handleAddItem = (item: DataInfo) => {
-        setItemList((prevData) => [...prevData, item]);
-      };
+	const handleAddItem = (item: DataInfo) => {
+		setItemList((prevData) => [...prevData, item]);
+	};
 
-    return(
-        <>
-           <Form onSubmitHandler={handleAddItem}/>
-           <Table itemList={itemList}/>
-           {/* {itemList.map((val) => { return(
+	return (
+		<>
+			<Form onSubmitProp={handleAddItem} />
+			<Table itemList={itemList} />
+			{/* {itemList.map((val) => { return(
                 <div key={val.cost}>
                     <span >{val.itemName} : {val.Category}</span>
                 </div>
                )}
            )} */}
-
-           
-        </>
-    )
+		</>
+	);
 }
-export default App_ExpenseTracker
+export default App_ExpenseTracker;
--- src/assets/tracker_components/Form.tsx
+++ src/assets/tracker_components/Form.tsx
@@ -1,71 +1,170 @@
-import React, { FormEvent, useRef } from 'react'
-import { DataInfo } from '../Utilities/interfaceTypes'
+import React from "react";
+import { DataInfo } from "../Utilities/interfaceTypes";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-
-//causes default state in app comopnent to return to default initiation
-interface Props{
-    onSubmitHandler: (item: DataInfo) => void
+interface IFormData {
+	firstname: string;
+	lastname: string;
 }
 
-const Form = ({onSubmitHandler}: Props) => {
-    const nameRef = useRef<HTMLInputElement>(null)
-    const costRef = useRef<HTMLInputElement>(null)
-    const categoryRef = useRef<HTMLSelectElement>(null)
-    const infoRef = useRef<HTMLInputElement>(null)
-    
-    const handleOnSubmit = (e: FormEvent) => {
-    e.preventDefault
-    let temp: DataInfo = {
-        itemName: "",
-        cost: 0,
-        Category: "",
-        info: ""
-    }
-       
-    temp.itemName =nameRef.current?.value || ""
-    if(costRef.current)
-        temp.cost = parseInt(costRef.current.value)
-    temp.info = infoRef.current?.value || ""
-    temp.Category = categoryRef.current?.value || "Not Categorized"
-    console.log(temp)
-    onSubmitHandler(temp)
-
+interface Props {
+	onSubmitProp: (data: DataInfo) => void;
 }
 
-  return (
-    <>
-        {/* <form> */}
-        <form onSubmit={handleOnSubmit}>
-            <div className='mb-3'>
-            <label htmlFor="itemName" className="form-label">Item Name
-                <input id='itemName' ref={nameRef} className="form-control" type='text' />
-                </label>
-                </div>
-                <div className='mb-3'>    
-            <label htmlFor="cost">Cost
-                <input id='cost' ref={costRef} type="number" className="form-control" />
-                </label>
-                </div>
-                <div className='mb-3'> 
-            <label htmlFor="info" className="form-label">Description
-                <input id='info' ref={infoRef} type="text" className="form-control" />
-                </label>
-                </div>
-                <div className='mb-3'> 
-            <label htmlFor="category" className="form-control">Category
-                <select id="category" ref={categoryRef}  defaultValue="Food" className="form-control">
-                    <option value={"Entertainment"}>Entertainment</option>
-                    <option value={"Food"}>Food</option>
-                </select>
-                </label>
-                </div>
-                <div className='mb-3'> 
-                {/* <button onClick={handleOnSubmit} type='button'>Submit</button> */}
-                <button type='submit'>Submit</button> 
-                </div>
-        </form>
-    </>
-  )
-}
+//using React-Hook-Forms
+const Form = ({ onSubmitProp }: Props) => {
+	const { register, handleSubmit } = useForm<IFormData>();
+	const onSubmit: SubmitHandler<IFormData> = (data) => {
+		const tempData: DataInfo = {
+			itemName: data.firstname,
+			cost: 12,
+			Category: "Something is better",
+			info: data.lastname,
+		};
+		console.log(data);
+		onSubmitProp(tempData);
+	};
+	// const handleOnSubmit = (data: IFormData) => {
+	// 	const tempData: DataInfo = {
+	// 		itemName: data.firstname,
+	// 		cost: 12,
+	// 		Category: "Something is better",
+	// 		info: data.lastname,
+	// 	};
+	// 	console.log(data);
+	// 	onSubmit(tempData);
+	// };
 
-export default Form
+	return (
+		<>
+			<form onSubmit={handleSubmit((data) => onSubmit(data))}>
+				<label htmlFor="firstname">
+					First Name
+					<input
+						{...register("firstname")}
+						type="text"
+						name="firstname"
+						id="firstname"
+					/>
+				</label>
+				<label htmlFor="lastname">
+					Last Name
+					<input
+						{...register("lastname")}
+						type="text"
+						name="lastname"
+						id="lastname"
+					/>
+				</label>
+				<button
+					className="btn btn-primary"
+					type="submit"
+				></button>
+			</form>
+		</>
+	);
+};
+
+export default Form;
+
+// useRef implimentation
+
+// interface Props {
+// 	onSubmitHandler: (item: DataInfo) => void;
+// }
+
+// const Form = ({ onSubmitHandler }: Props) => {
+// 	const nameRef = useRef<HTMLInputElement>(null);
+// 	const costRef = useRef<HTMLInputElement>(null);
+// 	const categoryRef = useRef<HTMLSelectElement>(null);
+// 	const infoRef = useRef<HTMLInputElement>(null);
+
+// 	const handleOnSubmit = (e: FormEvent) => {
+// 		e.preventDefault();
+// 		const temp: DataInfo = {
+// 			itemName: "",
+// 			cost: 0,
+// 			Category: "",
+// 			info: "",
+// 		};
+
+// 		temp.itemName = nameRef.current?.value || "";
+// 		if (costRef.current) {
+// 			temp.cost = parseInt(costRef.current.value);
+// 		}
+// 		temp.info = infoRef.current?.value || "";
+// 		temp.Category = categoryRef.current?.value || "Not Categorized";
+// 		console.log(temp);
+// 		onSubmitHandler(temp);
+// 	};
+
+// 	return (
+// 		<>
+// 			{/* <form> */}
+// 			<form onSubmit={handleOnSubmit}>
+// 				<div className="mb-3">
+// 					<label
+// 						htmlFor="itemName"
+// 						className="form-label"
+// 					>
+// 						Item Name
+// 						<input
+// 							id="itemName"
+// 							ref={nameRef}
+// 							className="form-control"
+// 							type="text"
+// 						/>
+// 					</label>
+// 				</div>
+// 				<div className="mb-3">
+// 					<label htmlFor="cost">
+// 						Cost
+// 						<input
+// 							id="cost"
+// 							ref={costRef}
+// 							type="number"
+// 							className="form-control"
+// 						/>
+// 					</label>
+// 				</div>
+// 				<div className="mb-3">
+// 					<label
+// 						htmlFor="info"
+// 						className="form-label"
+// 					>
+// 						Description
+// 						<input
+// 							id="info"
+// 							ref={infoRef}
+// 							type="text"
+// 							className="form-control"
+// 						/>
+// 					</label>
+// 				</div>
+// 				<div className="mb-3">
+// 					<label
+// 						htmlFor="category"
+// 						className="form-control"
+// 					>
+// 						Category
+// 						<select
+// 							id="category"
+// 							ref={categoryRef}
+// 							defaultValue="Food"
+// 							className="form-control"
+// 						>
+// 							<option value={"Entertainment"}>Entertainment</option>
+// 							<option value={"Food"}>Food</option>
+// 						</select>
+// 					</label>
+// 				</div>
+// 				<div className="mb-3">
+// 					{/* <button onClick={handleOnSubmit} type='button'>Submit</button> */}
+// 					<button type="submit">Submit</button>
+// 				</div>
+// 			</form>
+// 		</>
+// 	);
+// };
+
+// export default Form;
