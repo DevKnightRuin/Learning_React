8d969deb6ae161606dcde1be5735c9075fe39f62
nick <devknightruin@outlook.com>
2024-01-04 13:34:22 -0600

    converted form to utilize react-hook-form /w schema validation using zod

--- src/assets/tracker_components/Form.tsx
+++ src/assets/tracker_components/Form.tsx
@@ -1,65 +1,116 @@
-import React from "react";
 import { DataInfo } from "../Utilities/interfaceTypes";
 import { useForm, SubmitHandler } from "react-hook-form";
-
-interface IFormData {
-	firstname: string;
-	lastname: string;
-}
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Props {
 	onSubmitProp: (data: DataInfo) => void;
 }
+const schema = z.object({
+	name: z.string({ invalid_type_error: "The item requires a name" }).min(2),
+	cost: z.number().min(0.01),
+	info: z
+		.string({ invalid_type_error: "Information about this item is required" })
+		.min(3),
+	category: z.string(),
+});
+
+type ItemData = z.infer<typeof schema>;
 
 //using React-Hook-Forms
 const Form = ({ onSubmitProp }: Props) => {
-	const { register, handleSubmit } = useForm<IFormData>();
-	const onSubmit: SubmitHandler<IFormData> = (data) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<ItemData>({
+		resolver: zodResolver(schema),
+	});
+
+	const onSubmit: SubmitHandler<ItemData> = (data) => {
+		console.log("Form info: " + data.name);
 		const tempData: DataInfo = {
-			itemName: data.firstname,
-			cost: 12,
-			Category: "Something is better",
-			info: data.lastname,
+			itemName: data.name,
+			cost: data.cost,
+			Category: data.category,
+			info: data.info,
 		};
-		console.log(data);
+		console.log("state info: " + tempData);
 		onSubmitProp(tempData);
 	};
-	// const handleOnSubmit = (data: IFormData) => {
-	// 	const tempData: DataInfo = {
-	// 		itemName: data.firstname,
-	// 		cost: 12,
-	// 		Category: "Something is better",
-	// 		info: data.lastname,
-	// 	};
-	// 	console.log(data);
-	// 	onSubmit(tempData);
-	// };
 
 	return (
 		<>
 			<form onSubmit={handleSubmit((data) => onSubmit(data))}>
-				<label htmlFor="firstname">
-					First Name
-					<input
-						{...register("firstname")}
-						type="text"
-						name="firstname"
-						id="firstname"
-					/>
-				</label>
-				<label htmlFor="lastname">
-					Last Name
-					<input
-						{...register("lastname")}
-						type="text"
-						name="lastname"
-						id="lastname"
-					/>
-				</label>
+				{/* <form onSubmit={(data) => console.log(data)}> */}
+				<div className="mb-3">
+					<label
+						htmlFor="firstname"
+						className="form-label"
+					>
+						Product Name
+						<input
+							{...register("name")}
+							className="form-control"
+							type="text"
+							id="name"
+						/>
+						{errors.name && <p>{errors.name.message}</p>}
+					</label>
+				</div>
+				<div className="mb-3">
+					<label
+						htmlFor="lastname"
+						className="form-label"
+					>
+						Product Description
+						<input
+							{...register("info")}
+							className="form-control"
+							type="text"
+							id="lastname"
+						/>
+						{errors.info && <p>{errors.info.message}</p>}
+					</label>
+				</div>
+				<div className="mb-3">
+					<label
+						htmlFor="cost"
+						className="form-label"
+					>
+						<input
+							{...register("cost", { valueAsNumber: true })}
+							type="number"
+							id="Cost"
+							className="form-control"
+						/>
+						{errors.cost && <p>{errors.cost.message}</p>}
+					</label>
+				</div>
+				<div className="mb-3">
+					<label
+						htmlFor="category"
+						className="form-control"
+					>
+						Category
+						<select
+							{...register("category")}
+							id="category"
+							defaultValue="Food"
+							className="form-control"
+						>
+							<option value={"Entertainment"}>Entertainment</option>
+							<option value={"Food"}>Food</option>
+						</select>
+					</label>
+				</div>
+
 				<button
 					className="btn btn-primary"
 					type="submit"
-				></button>
+				>
+					Submit
+				</button>
 			</form>
 		</>
 	);
@@ -67,7 +118,7 @@ const Form = ({ onSubmitProp }: Props) => {
 
 export default Form;
 
-// useRef implimentation
+// useRef implementation
 
 // interface Props {
 // 	onSubmitHandler: (item: DataInfo) => void;
--- src/assets/tracker_components/Table.tsx
+++ src/assets/tracker_components/Table.tsx
@@ -1,44 +1,41 @@
-import React from 'react'
-import { DataInfo } from '../Utilities/interfaceTypes'
+import { DataInfo } from "../Utilities/interfaceTypes";
 
-interface Props{
-    itemList: DataInfo[]
+interface Props {
+	itemList: DataInfo[];
 }
 
-const Table = ({itemList}: Props) => {
-  return (
-    <>
-    <table className='table table-striped'>
-        <thead>
-            <tr> 
-                <th></th>
-                <th>Item</th>
-                <th>Cost</th>
-                <th>Category</th>
-                <th>Info</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-            {itemList.map((val, iter) => { return(
-                <tr key={iter}>
-                    <td>{iter}</td>
-                    <td>{val.itemName}</td>
-                    <td>{Number.isNaN(val.cost)? 0 : val.cost}</td>
-                    <td>{val.Category}</td>
-                    <td>{val.info}</td>
-                    <td>DELETE</td>
-                </tr>
-            )
-            })}
-            
+const Table = ({ itemList }: Props) => {
+	return (
+		<>
+			<table className="table table-striped">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Item</th>
+						<th>Cost</th>
+						<th>Category</th>
+						<th>Info</th>
+						<th>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					{itemList.map((val, iter) => {
+						return (
+							<tr key={iter}>
+								<td>{iter}</td>
+								<td>{val.itemName}</td>
+								<td>{Number.isNaN(val.cost) ? 0 : val.cost}</td>
+								<td>{val.Category}</td>
+								<td>{val.info}</td>
+								<td>DELETE</td>
+							</tr>
+						);
+					})}
+				</tbody>
+				{/* map over an array to fill in the rest */}
+			</table>
+		</>
+	);
+};
 
-        </tbody>
-        {/* map over an array to fill in the rest */}
-    </table>
-    
-    </>
-  )
-}
-
-export default Table
+export default Table;
