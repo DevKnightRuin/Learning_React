cc8b0def60dc10b3618b98591a810624337c0118
nick <devknightruin@outlook.com>
2024-01-31 16:08:52 -0600

    added add/patch/delete functions and extracted the apicalls from the main component

--- src/App_FetchingData.tsx
+++ src/App_FetchingData.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import axios, { AxiosError, CanceledError } from "axios";
+import apiClient, { CanceledError } from "./assets/Utilities/apiClient";
 
 interface User {
 	name: string;
@@ -8,22 +7,27 @@ interface User {
 }
 
 const App_FetchingData = () => {
-	const [user, setUser] = useState<User[]>([]);
+	const [users, setUsers] = useState<User[]>([]);
 	const [error, setError] = useState<string>("");
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		const controller = new AbortController();
-
-		axios
-			.get<User[]>("https://jsonplaceholder.typicode.com/users", {
+		setIsLoading(true);
+		apiClient
+			.get<User[]>("/users", {
 				signal: controller.signal,
 			}) //we add the controller in the config section of the function
-			.then((res) => setUser(res.data)) //response from the fetch and store into state
+			.then((res) => {
+				setUsers(res.data); //response from the fetch and store into state
+				setIsLoading(false);
+			}) //set is loading to false, use .finally when not testing
 			.catch((err) => {
 				if (err instanceof CanceledError) return; //if the error is from a canceled error from the controller, just return so we don't display canceled on the screen
-				setError(err.message);
-			}); //catch any errors and store it in state
-
+				setError(err.message); //catch any errors and store it in state
+				setIsLoading(false); //set is loading to false, use .finally when not testing
+			});
+		// .finally(() => setIsLoading(false)); // ------  This does not show properly when using strict mode
 		return () => controller.abort(); //cleanup function to cancel the fetch request if this component unmounts
 	}, []);
 
@@ -42,12 +46,84 @@ const App_FetchingData = () => {
 	// 	}
 	// }, []);
 
+	//functions
+	function addUser() {
+		const newUser = { id: 0, name: "Pinochio" };
+		apiClient
+			.post("https://jsonplaceholder.typicode.com/users/", newUser)
+			.then((res) => setUsers([...users, res.data]))
+			.catch((err) => {
+				setError(err.message);
+			});
+	}
+
+	function deleteUser(user: User) {
+		const originalUsers = [...users];
+		setUsers(users.filter((u) => u.id !== user.id));
+		apiClient
+			.delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+			.catch((err) => {
+				setUsers(originalUsers);
+				setError(err.message);
+			});
+	}
+
+	function updateUser(user: User) {
+		const originalUsers = [...users];
+		const updatedUser = { ...user, name: user.name + "!" };
+		setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+		apiClient
+			.patch(
+				"https://jsonplaceholder.typicode.com/users/" + user.id,
+				updateUser
+			)
+			.catch((err) => {
+				setError(err.message);
+				setUsers(originalUsers);
+			});
+	}
+
+	function UID() {
+		return Math.floor(Math.random() * 298374);
+	}
+
 	return (
 		<>
 			<h1>App_FetchingData</h1>
 			{error && <p className="text-danger">{error}</p>}
-			{user.map((user) => (
-				<li key={user.id}>{user.name} </li>
+			{isLoading && <div className="spinner-border"></div>}
+			<button
+				className="btn btn-primary m-2"
+				onClick={addUser}
+			>
+				Add User
+			</button>
+			{users.map((user) => (
+				<ul
+					key={UID()}
+					className="list-group"
+				>
+					<li
+						key={user.id}
+						className="list-group-item d-flex justify-content-between"
+					>
+						{user.name}
+						<div>
+							<button
+								className="btn btn-outline-danger mx-1"
+								onClick={() => updateUser(user)}
+							>
+								Modify
+							</button>
+							<button
+								className="btn btn-outline-danger mx-1"
+								onClick={() => deleteUser(user)}
+							>
+								Delete
+							</button>
+						</div>
+					</li>
+				</ul>
 			))}
 		</>
 	);
